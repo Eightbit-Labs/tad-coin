c1410c5e4698969fea8a616e1b44f6e5cb8e402e
Lawrence Tong <lawrencetong1030@gmail.com>
2026-05-27 17:46:40 +0200

    stupid frikin react modules not installed

--- src/server/src/routes/transfer.ts
+++
@@ -1,6 +0,0 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-import { getDb } from '../db';
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import { useState, useRef, useEffect } from 'react';
